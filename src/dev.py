504fa857e5113440075cf03b4859589015331b1c
AmOs15 <amos.game1615@gmail.com>
2025-04-09 22:47:47 +0900

    feat: step2

--- src/dev.py
+++ src/dev.py
@@ -1,13 +1,20 @@
 import numpy as np
-from core import Variable
+from core import Variable, Square
 
 def step1():
     data = np.array([1.0, 2.0, 3.0])
     x = Variable(data)
     print(x.data)
 
+def step2():
+    x = Variable(np.array(10))
+    square = Square()
+    y = square(x)
+    print(type(y))
+    print(y.data)
+
 def main():
-    step1()
+    step2()
 
 if __name__ == "__main__":
     main()
